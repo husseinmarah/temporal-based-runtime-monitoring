aa8b73bed593ec5b7be912e8bc7a37625f96f3a3
Hussein Marah <hussein.marah@ymail.com>
2021-07-13 16:06:34 +0200

    Delete monitoring_framework/at.jku.se.mosumo.dronology.constraint5 directory

--- monitoring_framework/at.jku.se.mosumo.dronology.constraint5/src/main/java/at/jku/se/mosumo/dronology/constraint/subscriber/StatementSubscriber.java
+++
@@ -1,9 +0,0 @@
-
-package at.jku.se.mosumo.dronology.constraint.subscriber;
-public interface StatementSubscriber {
-    /**
-     * Get the EPL Stamement the Subscriber will listen to.
-     * @return EPL Statement
-     */
-    public String getStatement();
-}
--- monitoring_framework/at.jku.se.mosumo.dronology.constraint5/src/main/java/at/jku/se/mosumo/dronology/constraint/subscriber/VoltageEventSubscriber.java
+++
@@ -1,8 +0,0 @@
-package at.jku.se.mosumo.dronology.constraint.subscriber;
-public class VoltageEventSubscriber implements StatementSubscriber {
-    public String getStatement() {  	
-    	String VoltageEventStatement = "select * from VoltageEvent(voltage > 50)";
-        return VoltageEventStatement;
-    }
-  
-}
